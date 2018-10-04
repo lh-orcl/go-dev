a8459584b526ce79d5801a1a3a5c3f8858f426c5
Luke Hutton <luke.hutton@oracle.com>
2018-10-04 11:25:48 +0100

    replacement all!

--- trimming-spaces/replace-strings.go
+++ trimming-spaces/replace-strings.go
@@ -8,5 +8,7 @@ import (
 func main() {
 	helloWorld := "Hello, World, World, World"
 	helloMars := strings.Replace(helloWorld, "World", "Mars", 2)
+	helloLots := strings.Replace(helloWorld, "World", "Mars", -1)
 	fmt.Println(helloMars)
+	fmt.Println(helloLots)
 }
