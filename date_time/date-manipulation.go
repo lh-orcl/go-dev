7f3793fe31797f8c51449776109f8c53f11a8849
Luke Hutton <luke.hutton@oracle.com>
2018-10-05 10:45:47 +0100

    date additions

--- date_time/date-manipulation.go
+++ date_time/date-manipulation.go
@@ -13,4 +13,10 @@ func main() {
 	fmt.Println(current.String())
 	fmt.Println(nextMonth.String())
 	fmt.Println(nextYear.String())
+
+	lastYear := current.AddDate(-1, 0, 0)
+	fmt.Println(lastYear.String())
+
+	nextMinute := current.Add(1 * time.Minute)
+	fmt.Println(nextMinute.String())
 }
