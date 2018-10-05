a0e5c8d9cdef5e450da5f10a272cf38f842b3d4c
Luke Hutton <luke.hutton@oracle.com>
2018-10-05 10:39:52 +0100

    date manipulation

---
+++ date_time/date-manipulation.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	current := time.Now()
+	nextMonth := current.AddDate(0, 1, 0)
+	nextYear := current.AddDate(1, 0, 0)
+
+	fmt.Println(current.String())
+	fmt.Println(nextMonth.String())
+	fmt.Println(nextYear.String())
+}
