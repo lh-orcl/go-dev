a1bd4d9695a9fc210dd0c1960991ac862466f45c
Luke Hutton <luke.hutton@oracle.com>
2018-10-15 11:52:32 +0100

    temp files

---
+++ files-directories/file-temp.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	helloWorld := "Hello, World"
+	file, err := ioutil.TempFile("", "hello_world_temp")
+	if err != nil {
+		panic(err)
+	}
+	defer os.Remove(file.Name())
+	if _, err := file.Write([]byte(helloWorld)); err != nil {
+		panic(err)
+	}
+	fmt.Println(file.Name())
+}
